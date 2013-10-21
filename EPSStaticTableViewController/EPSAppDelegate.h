0bc66d92c9c4b970ff02c741afac6068910c23d4
Peter Stuart <peter@electricpeelsoftware.com>
2013-10-21 14:31:44 -0400

    Initial Commit

---
+++ EPSStaticTableViewController/EPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EPSAppDelegate.h
+//  EPSStaticTableViewController
+//
+//  Created by Peter Stuart on 10/21/13.
+//  Copyright (c) 2013 Electric Peel, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
